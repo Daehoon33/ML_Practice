9c5ddd56f19701951375b3490479c616a79eef8e
dkim <kim.daehoon03@gmail.com>
2020-10-23 20:22:05 +0900

    test for commit

--- main.py
+++ main.py
@@ -17,3 +17,10 @@ print("NumPy array:\n{}".format(e))
 # convert the numpy array to a SciPy matrix in CSR format
 sparse_mt = sparse.csr_matrix(e)
 print('\nSciPy Sparse CSR matrix:\n{}'.format(sparse_mt))
+
+# Create Coo Matrix
+dt = np.ones(4)
+row_indices = np.arange(4)
+col_indices = np.arange(4)
+eye_coo = sparse.coo_matrix((dt, (row_indices, col_indices)))
+print('COO representation:\n{}'.format(eye_coo))
