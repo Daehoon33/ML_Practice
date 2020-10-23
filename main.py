ffb26337de156b0c5c7a594664946eeedc27f6b9
DaeHoon Kim (KR-Strategic Planning-Consultant) <dkim@compassion.or.kr>
2020-10-23 20:03:28 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# use package
+
+# Numpy
+import numpy as np
+
+x = np.array([[1,2,3], [4,5,6]])
+print("x:\n{}".format(x))
+
+# sciPy
+
+from scipy import sparse
+
+# Create a 2D array with a diagonal of ones, and zeros everywhere are
+e = np.eye(4)
+print("NumPy array:\n{}".format(e))
+
+# convert the numpy array to a SciPy matrix in CSR format
+sparse_mt = sparse.csr_matrix(e)
+print('\nSciPy Sparse CSR matrix:\n{}'.format(sparse_mt))
